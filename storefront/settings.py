732efabc8ac26be1117613ca6f835556e5f0ddee
Sankhadip Das <sankhadip10.das@gmail.com>
2025-07-01 05:56:55 +0530

    Added the users

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,13 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'playground',
     'debug_toolbar',
     'store',
-    'store_custom',
     'tags',
-    'likes'
-
+    'likes',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -134,11 +134,26 @@ INTERNAL_IPS = [
     # ...
 ]
 
-REST_FRAMEWORK ={
+REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'PAGE_SIZE': 10
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
+
+AUTH_USER_MODEL = 'core.User'
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer'
+    }
 }
 
 
